5bc49f87cfeae6dbf99299f8c5abf17eebfb030f
steve <stevetasticsteve@gmail.com>
2024-11-26 20:43:58 +1000

    added .env for production

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,14 +6,13 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "lexicon_app_db",
         "USER": "django",
-        "PASSWORD": "1234",
+        "PASSWORD": os.getenv("DATABASE_PASSWORD", "1234"),
         "HOST": "db",
-        "PORT": "",
+        "PORT": 5432,
     }
 }
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-24#+mypq*=1v77s(37v+_$t!p7+iwdnq)$q&djz85vo$9f5sym"
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-24#+mypq*=1v77s(37v+_$t!p7+iwdnq)$q&djz85vo$9f5sym")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
