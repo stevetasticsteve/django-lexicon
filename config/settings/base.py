30fee614b8e64354f4e713f08160c9975082fb13
steve <stevetasticsteve@gmail.com>
2024-12-04 12:05:39 +1000

    forgot to makemigrations

--- config/settings/base.py
+++ config/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-version = "0.1.2"
+version = "0.1.3"
 
 # Application definition
 
