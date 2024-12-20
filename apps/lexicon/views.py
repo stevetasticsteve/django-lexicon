16cc4a5ff507bc62bb77c6b3978bd6aa43bc5dba
steve <stevetasticsteve@gmail.com>
2024-12-20 12:25:09 +1000

    added English search

--- apps/lexicon/views.py
+++ apps/lexicon/views.py
@@ -81,6 +81,12 @@ class SearchResults(ListView):
 
     def get_queryset(self, **kwargs):
         search = self.request.GET.get("search")
+        is_english = self.request.GET.get("eng") == "true"
+
+        # Determine the field to search based on `is_english`
+        search_field = "eng__icontains" if is_english else "tok_ples__icontains"
+        filter_kwargs = {search_field: search}
+
         self.project = get_object_or_404(
             models.LexiconProject, language_code=self.kwargs.get("lang_code")
         )
@@ -88,7 +94,7 @@ class SearchResults(ListView):
             project=self.project
         )
         if search:
-            return query.filter(tok_ples__icontains=search)
+            return query.filter(**filter_kwargs)
         else:
             return query
 
